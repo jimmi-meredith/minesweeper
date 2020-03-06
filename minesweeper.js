f413bfcff241a41de3299f0c85a75a63d6e4ce2f
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-03-06 14:04:31 +1300

    Add reset button

--- minesweeper.js
+++ minesweeper.js
@@ -1,73 +1,38 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-var board = {
-  cells: [
-    {
-      row: 0, 
-      col: 0, 
-      isMine: true, 
-      hidden: true
-    },  
-    {
-      row: 0, 
-      col: 1, 
-      isMine: false, 
-      hidden: true
-    }, 
-    {
-      row: 0, 
-      col: 2, 
-      isMine: false, 
-      hidden: true
-    },
-    {
-      row: 1, 
-      col: 0, 
-      isMine: false, 
-      hidden: true
-    },
-    {
-      row: 1, 
-      col: 1, 
-      isMine: true, 
-      hidden: true
-    },    
-    {
-      row: 1, 
-      col: 2, 
-      isMine: false, 
-      hidden: true
-    }, 
-    {
-      row: 2, 
-      col: 0, 
-      isMine: false, 
-      hidden: true
-    },
-    {
-      row: 2, 
-      col: 1, 
-      isMine: false, 
-      hidden: true
-    },
-    {
-      row: 2, 
-      col: 2, 
-      isMine: true, 
-      hidden: true
-    },
-  ]
+var  board = {};
+document.addEventListener('click', checkForWin)
+document.addEventListener('contextmenu', checkForWin)
+
+function createBoard(num){
+  var cells = []
+  var cellNum = 0
+  for(y = 0; y < num; y ++){
+    for(x = 0; x < num; x++){
+      cells[cellNum] = {
+        row: y,
+        col: x,
+        isMine: (Math.floor(Math.random() * 3) ) == 2,
+        hidden: true
+      }
+      cellNum ++
+    }
+  }
+  return cells
 }
 
 function startGame() {
+  board.cells = createBoard(5)
   for (i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }  
+
+  var reset = document.querySelector("#reset")
+  reset.addEventListener("click", resetBoard);
+
+
   lib.initBoard()
 }
-
-document.addEventListener('click', checkForWin)
-document.addEventListener('contextmenu', checkForWin)
 
 function checkForWin() {
   var winCount = 0
@@ -95,4 +60,10 @@ function countSurroundingMines(cell) {
     }
   } 
   return count
+}
+
+
+function resetBoard() {
+  document.getElementsByClassName("board")[0].innerHTML = ""
+  startGame();
 }
